4d4eee481b4250ac1c98b839b80ae7e5745409f3
padmavathi <padmavathichilamala@gmail.com>
2024-03-05 11:58:42 +0530

    attributecreation

---
+++ Attributescreation.cs
@@ -0,0 +1,36 @@
+﻿/*using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProject
+{
+    class AddressBook
+    {
+        public static List<AddressBook> listobj = new List<AddressBook>();
+        public string Id { get; set; }    //Assigning the values
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Zipcode { get; set; }
+        public string MobileNumber { get; set; }
+        public string Email { get; set; }
+
+        
+
+    }
+
+    class MainAddressBook
+    {
+
+        public static void Main(string[] args)
+        {
+            AddressBook obj = new AddressBook();
+
+        }
+    }
+}
+*/
